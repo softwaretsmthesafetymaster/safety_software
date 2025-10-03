260c3c486c69ebf966b8db28e3d2b7ec913cbe45
softwaretsmthesafetymaster <softwaretsmthesafetymaster@gmail.com>
2025-10-03 15:46:41 +0530

    Updated Button.tsx

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import { LucideIcon } from "lucide-react";
+import { Video as LucideIcon } from "lucide-react";
 import LoadingSpinner from "./LoadingSpinner";
 
 interface ButtonProps {
